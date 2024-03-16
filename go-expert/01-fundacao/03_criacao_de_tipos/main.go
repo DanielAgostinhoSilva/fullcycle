e410d5e8745b23dfbf45f53c697f0e6ee5b5845d
Daniel <daniel.agostinho@hotmail.com>
2024-03-16 18:45:19 -0300

    feat: 03_criacao_de_tipos

---
+++ go-expert/01-fundacao/03_criacao_de_tipos/main.go
@@ -0,0 +1,24 @@
+package main
+
+const (
+	a = "Hello World"
+)
+
+type ID int
+
+var (
+	b bool = true
+	c int
+	d string = "teste d"
+	e float64
+	f ID = 1
+)
+
+func main() {
+	println(a)
+	println(b)
+	println(c)
+	println(d)
+	println(e)
+	println(f)
+}
