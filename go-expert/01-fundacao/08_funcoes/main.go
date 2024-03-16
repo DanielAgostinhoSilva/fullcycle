50a405db58534cc7f26b08a8948359d357680fef
Daniel <daniel.agostinho@hotmail.com>
2024-03-16 20:15:17 -0300

    feat: 08_funcoes

---
+++ go-expert/01-fundacao/08_funcoes/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+func main() {
+	valor, err := sum(1, 2)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(valor)
+
+	valor, err = sum(10, 50)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(valor)
+}
+
+func sum(a, b int) (int, error) {
+
+	if a+b >= 50 {
+		return a + b, errors.New("A soma é maior que 50")
+	}
+	return a + b, nil
+}
